7eda10103fa7cf65dd9684f3cebc493031391c40
chidoski <chidikalu@gmail.com>
2017-07-25 22:59:57 -0400

    Add files via upload

---
+++ isOdd.js
@@ -0,0 +1,15 @@
+function isOdd(num){
+  if(!(num % 2 == 0))
+  {
+    console.log("This number is odd.");
+  }
+  else {
+    console.log("This number is even.");
+  }
+}
+
+isOdd(2);
+isOdd(5);
+isOdd(6);
+isOdd(1);
+isOdd(2);
